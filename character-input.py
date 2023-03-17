bd335aa0d549391b3e03844d935a3710af0b2425
dougienmysnuggy <leonardw@gmail.com>
2023-03-17 18:11:33 -0400

    Added 4 exercises from PracticePython.org

---
+++ character-input.py
@@ -0,0 +1,25 @@
+"""
+Create a program that asks the user to enter their name and their age. 
+Print out a message addressed to them that tells them the year that they 
+will turn 100 years old. Note: for this exercise, the expectation is 
+that you explicitly write out the year (and therefore be out of date the 
+next year). If you want to do this in a generic way, see exercise 39.
+
+Extras:
+
+Add on to the previous program by asking the user for another number and 
+printing out that many copies of the previous message. (Hint: order of 
+operations exists in Python)
+
+Print out that many copies of the previous message on separate lines. 
+(Hint: the string "\n is the same as pressing the ENTER button)
+"""
+
+YEAR = 2023
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+favorite_number = input('Enter your favorite number: ')
+answer = (100 - int(age)) + YEAR
+print(int(favorite_number) * ('Hi ' + name + '! You will turn 100 years old in ' + str(answer) + '.\n'))
+
+
---
+++ list-less-than-ten.py
@@ -0,0 +1,30 @@
+"""
+Take a list, say for example this one:
+
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+and write a program that prints out all the elements of the list that are less than 
+5.
+
+Extras:
+
+Instead of printing the elements one by one, make a new list that has all the 
+elements less than 5 from this list in it and print out this new list.
+
+Write this in one line of Python.
+
+Ask the user for a number and return a list that contains only elements from the 
+original list a that are smaller than that number given by the user.
+"""
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = []
+
+max_value = input('Enter maximum value of results: ')
+
+for x in a:
+    if x <= int(max_value):
+        b.append(x)
+
+for x in b:
+    print(str(x))
