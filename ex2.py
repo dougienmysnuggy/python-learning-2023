0fb765c8bd78f2fcfca9d935832a6ceb9be9bafe
dougienmysnuggy <leonardw@gmail.com>
2023-03-21 16:33:56 -0400

    updated weight conversion to go both ways

--- ex2.py
+++ ex2.py
@@ -1,3 +1,10 @@
 weight = input('Enter your weight (lbs): ')
-kg_weight = int(weight) * 0.453592
-print('Your weight in Kilograms is ' + str(kg_weight))
+choice = input('(L)bs or (K)ilos?: ')
+if choice.upper() == "L":
+    converted_weight = int(weight) * 0.45
+    print(f'Your weight in Kilograms is {converted_weight}.')
+elif choice.upper() == "K":
+    converted_weight = int(weight) / 0.45
+    print(f'Your weight in Pounds is {converted_weight}.')
+else:
+    print('Invalid input. Try again!')
